4a1dbd13b45c5c6ea85bbb1201dc819bf76d97ba
Trey Allen Gower <tagower@login1.ls6.tacc.utexas.edu>
2023-10-26 12:15:22 -0500

    Py scripts updated

--- Scripts/Animate.py
+++ Scripts/Animate.py
@@ -3,6 +3,8 @@ import numpy as np
 from PIL import Image
 image_frames_T = []
 image_frames_P = []
+image_frames_Press = []
+
 
 hours = 132
 
@@ -13,14 +15,21 @@ for i in range(0,hours,6):
 
     new_frame_P = PIL.Image.open(r'/corral/utexas/hurricane/tgower/TempCorrelation/Plots/Precip_plots/Precip_hr' + str(i) + '.0.jpg')
     image_frames_P.append(new_frame_P)
+    
+    new_frame_Press = PIL.Image.open(r'/corral/utexas/hurricane/tgower/TempCorrelation/Plots/Press_plots/Press_hr' + str(i) + '.0.jpg')
+    image_frames_Press.append(new_frame_Press)
 
-
-image_frames_T[0].save('Temperature_Timelapse.gif', format = 'GIF',
+image_frames_T[0].save('/corral/utexas/hurricane/tgower/TempCorrelation/Plots/Temperature_Timelapse.gif', format = 'GIF',
         append_images = image_frames_T[1:],
         save_all =True, duration=350, 
         loop=0)
 
-image_frames_P[0].save('Precip_Timelapse.gif', format = 'GIF',
+image_frames_P[0].save('/corral/utexas/hurricane/tgower/TempCorrelation/Plots/Precip_Timelapse.gif', format = 'GIF',
         append_images = image_frames_P[1:],
+        save_all =True, duration=350,
+        loop=0)
+
+image_frames_Press[0].save('/corral/utexas/hurricane/tgower/TempCorrelation/Plots/Pressure_Timelapse.gif', format = 'GIF',
+        append_images = image_frames_Press[1:],
         save_all =True, duration=350,
         loop=0)
