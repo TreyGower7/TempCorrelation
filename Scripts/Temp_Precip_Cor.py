aad4581e6d292f37ac92b7708ee6c04ff03c8971
Trey Allen Gower <tagower@login1.ls6.tacc.utexas.edu>
2023-11-09 10:44:29 -0600

    dimensions not matching

--- Scripts/Temp_Precip_Cor.py
+++ Scripts/Temp_Precip_Cor.py
@@ -28,23 +28,24 @@ def find_dp(Precip_ds):
 
 
 def correlation_coeff(t,T,dP):
-  """ Calculate correlation between Temperature and Precipitation
+    """ Calculate correlation between Temperature and Precipitation
 
      Args: t(List containing two values) to acces a specific time range, Temperature dataset, and precipitation dataset
 
      Returns: List containing correlation coefficients for the full set and sliced sets
 
     """
+    CTP = []
     #adjust for time values given
     if type(t) != list:
         #setting default t to whole dataset for any parameter that is not an input range
         t = [1,43]
 
     #Updated every 3 hours
-    for i in range(t[0],t[1]),1):
+    for i in range(t[0],t[1],1):
         #Calculate Correlation
-        
-        CTP = np.dot(T,dP) / np.sqrt(np.dot(T,T)*np.dot(dP,dP)) 
+        CTP.append(np.dot(T[i],dP[i]) / np.sqrt(np.dot(T[i],T[i])*np.dot(dP[i],dP[i]))) 
+    
     return CTP
 
 def main():
@@ -55,7 +56,9 @@ def main():
     path1 = '/corral/utexas/hurricane/tgower/har_dataset_02/Precipt_HHar_d02.nc'
     ds_P = xr.open_dataset(path1)
 
-    # Access a specific variable
+    print(ds_T.keys())
+    print(ds_P.keys())
+# Access a specific variable
     print("\n...Getting Temperature and Precipitation Data...")
     print("_________________________________________________\n")
 
@@ -67,20 +70,18 @@ def main():
     dp = find_dp(Precip_data)    
    
     #Subset of the Temperature Data
-    #T_sub = []
+    T_sub = []
     #index by 12 for data every 3 hours
-    #for i in range(0,528,12):
-    #    T_sub.append(Temp_data[i])
+    for i in range(0,528,12):
+        T_sub.append(Temp_data[i][0])
 
-    # Convert the list of differences to an xarray DataArray
-    #T_xr = xr.concat(T_sub, dim='time')
+    #Convert the list of differences to an xarray DataArray
+    T_xr = xr.concat(T_sub, dim='time')
 
-    print(T_xr)
-    print(dp)
 # Correlation
     print("...Finding Correlation Coefficient for each 3 hour time step...")
     print("_________________________________________________\n")
-    correlation = correlation_coeff(0,Temp_data,dp)
+    correlation = correlation_coeff(0,T_xr,dp)
     
     #print correlation at each 3 hour time step
     for i in range(len(correlation)):
