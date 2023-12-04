53467fb136b8529ea06e70d41bdd468a2a236cb3
Trey Allen Gower <tagower@login2.ls6.tacc.utexas.edu>
2023-12-04 12:03:16 -0600

    test and correlations work?

--- Scripts/Temp_Precip_Cor.py
+++ Scripts/Temp_Precip_Cor.py
@@ -8,7 +8,24 @@ Caluclating Correlation Coefficients for Temperature and Precipitation data
 
 __author__ = "Trey Gower"
 
+def rm_zeros(dp,T):
+    """ Removing zero values of precipitation and the corresponding temperature values
+
+     Args: Precipitation and temperature at time t = i for every 3 hour time step
+
+     Returns: Two new adjusted numpy arrays excluding zero values of precipitation
+
+    """
+    #using numpy optimized algorithm to get the indices of the zeros
+    #for i in range(len(dp)):
+    zero_indices = np.where(dp == 0)
+    new_dp = np.delete(dp, zero_indices)
+    new_T = np.delete(T, zero_indices)
+    
+    return new_dp, new_T 
+
 def find_dp(Precip_ds):
+
     """ Calculate change in Precipitation data for every 3 hour time step
 
      Args: Precipitation dataset
@@ -19,37 +36,33 @@ def find_dp(Precip_ds):
     dp = []
     #index by 12 for data every 3 hours
     for i in range(0,528,12):
-        if i >=0 && Precip_ds[i].values != 0:
+        if i >=0:
             dp.append(Precip_ds[i]-Precip_ds[i-12])
 
     # Convert the list of differences to an xarray DataArray
     dp_xr = xr.concat(dp, dim='time')
     return dp_xr
    
-def mean(values):
-    return sum(values) / len(values)
+#def mean(values):
+#    return sum(values) / len(values)
 
-#calculate the correlation coefficient
-def ccf(T, dp):
-    
-    #To account for the zeros in precipitation and to ensure indices between Temp and Precipitation are uniform we can calculate
-    #the mean for temp and precipitation with the same for loops to ensure wherever Precipitation is zero the temperature value at that point 
-    #is not getting added into the equation
+#calculate the correlation coefficient (This works but is less optimal)
+#def ccf(T, dp):
+       
+#    x_mean = mean(T)
+#    y_mean = mean(dp)
 
-    x_mean = mean(T)
-    y_mean = mean(dp)
+#    numer = sum((xi - x_mean) * (yi - y_mean) for xi, yi in zip(T, dp))
+#    denom_T = sum((xi - x_mean)**2 for xi in T)
+#    denom_dp = sum((yi - y_mean)**2 for yi in dp)
 
-    numer = sum((xi - x_mean) * (yi - y_mean) for xi, yi in zip(T, dp))
-    denom_T = sum((xi - x_mean)**2 for xi in T)
-    denom_dp = sum((yi - y_mean)**2 for yi in dp)
+#    correlation = numer / np.sqrt(denom_T * denom_dp)
 
-    correlation = numer / np.sqrt(denom_T * denom_dp)
+#    return correlation
 
-    return correlation
-
-def txt_w(ctp,num):
-    f = open(f'Correlation_Coefficients{num}.txt','w')
-    for i in range(len(ctp)):
+def txt_w(ctp):
+    f = open(f'Correlation_Coefficients.txt','w')
+    for i in range(1,len(ctp),1):
         f.write(f't (hours) = {i*3}: Correlation Coefficient = {ctp[i]}\n\n')
     f.close()
 
@@ -71,9 +84,17 @@ def main():
     print("...Populating Subsets...")
     print("_________________________________________________\n")
     dp = find_dp(Precip_data)    
-    
-    print(Temp_data[0][0].dims)
-    print(dp[0].dims)
+
+    variable_names_P = ds_P.keys()
+    variable_names_T = ds_T.keys()
+
+#Key information
+    print("\nKey Names: ")
+    print(str(variable_names_P) + "\n")
+    print("\nKey Names: ")
+    print(str(variable_names_T) + "\n")
+
+
 # Correlation
     print("...Finding Correlation Coefficient for each 3 hour time step...")
     print("_________________________________________________\n")
@@ -81,24 +102,17 @@ def main():
     ctp = []
     for i in range(len(dp)):
         if i != 0:
-            ctp.append(np.corrcoef(Temp_data[i+12][0].values.flatten(),dp[i].values.flatten())[0,1])
+            print('...Removing zeros for t = ' + str(i*3) + 'hrs...')
+            dpnew, Tnew = rm_zeros(dp[i].values.flatten(),Temp_data[i+12][0].values.flatten())
+            ctp.append(np.corrcoef(Tnew,dpnew)[0,1])
         else:
-            ctp.append(np.corrcoef(Temp_data[0][0].values.flatten(),dp[i].values.flatten())[0,1])
-    
-    ctp2 = []
-    for i in range(len(dp)):
-        if i != 0:
-            ctp2.append(ccf(Temp_data[i+12][0].values.flatten(),dp[i].values.flatten()))
-        else:
-            ctp2.append(ccf(Temp_data[0][0].values.flatten(),dp[0].values.flatten()))
+            continue
 
 # Write outputs to text file
-    txt_w(ctp,1)
-    txt_w(ctp2,2)
-    txt_w(ctp3,3)
+    txt_w(ctp)
 
     #print correlation at each 3 hour time step
-    print('Coefficients saved to Correlation_Coefficients text files')
+    print('Coefficients saved to Correlation_Coefficients.txt file')
     #close datasets
     ds_T.close()
     ds_P.close()
@@ -106,6 +120,7 @@ def main():
 if __name__ == "__main__":
     """ This is executed when run from the command line """
     main()
+
 
 
 
--- Scripts/t2.py
+++ Scripts/t2.py
@@ -13,27 +13,40 @@ def mean(dp):
     mean = result/L
     return mean
 
-original_array = np.array([1, 0, 2, 0, 3, 0, 4])
+def rm_zeros(dp):
+    """ Removing zero values of precipitation and the corresponding temperature values
+
+     Args: Precipitation and temperature at time t = i for every 3 hour time step
+
+     Returns: Two new adjusted numpy arrays excluding zero values of precipitation
+
+    """
+    
+    #using numpy optimized algorithm to get the indices of the zeros
+    zero_indices = np.where(dp[i] == 0)
+    new_dp = np.delete(dp[i], zero_indices)
+
+        #np.concatenate(new_dp, dp[i])
+   #    ndp.append(new_dp)
+         #dpn = xr.concat(new_dp) 
+   # ndp = xr.DataArray(ndp, dims=('p', 'r'))
+
+    return dp
+
 
 fn = '/corral/utexas/hurricane/tgower/har_dataset_02/Precipt_HHar_d02.nc'
 dataset = xr.open_dataset(fn)
 ds = dataset['RAIN_tot'][48]
 
-# Replace zero values with NaN
-ds_no_zeros = ds.where(ds != 0)
+dp = ds.values.flatten()
+print(len(dp))
+zero_indices = np.where(dp == 0)
+new_dp = np.delete(dp, zero_indices)
+print(len(new_dp))
 
-# Perform calculations on the modified dataset
-result = ds_no_zeros.mean(dim=('p','r'))
+#newdp =  rm_zeros(ds)
 
+#print(newdp)
 
-print("Original Dataset:")
-print(ds)
-
-print("\nModified Dataset (Zeros replaced with NaN):")
-#print(ds_no_zeros)
-
-print("\nResult of calculations excluding zeros:")
-print(result)
-
-m = mean(ds)
-print(m)
+#m = mean(ds)
+#print(m)
