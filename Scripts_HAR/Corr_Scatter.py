331b47ad78fd0c13a469acf9b9d991ae4bbcdab2
Trey Allen Gower <tagower@login1.ls6.tacc.utexas.edu>
2024-01-23 12:10:46 -0600

    code produces right plot

--- Scripts_HAR/Corr_Scatter.py
+++ Scripts_HAR/Corr_Scatter.py
@@ -34,13 +34,13 @@ def high_low(coeff, time):
     t_high = []
 
     for i in range(len(coeff)):
-        if int(coeff[i]) >= .3:
+        if abs(float(coeff[i])) >= .3:
             high.append(coeff[i])
             t_high.append(time[i])
         else:
             low.append(coeff[i])
             t_low.append(time[i])
-
+    
     return high, t_high, low, t_low
 
 def create_plot(coeff, time):
@@ -50,20 +50,19 @@ def create_plot(coeff, time):
      
      Returns: Scatter Plot
     """
-    #plotting coefficients vs time
-    x = time
-    y = coeff
+    High, t_high, Low, t_low = high_low(coeff, time)
 
-    high, t_high, low, t_low = high_low(coeff,time)
-
-    plt.plot(t_high, high, label='Coeffs', color = 'red')
-    plt.plot(t_low, low, label='Coeffs', color = 'blue')
-
+    plt.scatter(t_high, High, color='red', label = 'High Correlation')
+    plt.show()
+    plt.scatter(t_low, Low, color='blue', label = 'Low Correlation')
+    
     plt.legend()
 
     plt.xlabel('Time (hrs)')
     plt.ylabel('Coefficients')
-    plt.title('Non-coarse Coefficient Scatter')
+    plt.title('Coarse/Non-Coarse Coefficient Scatter')
+    
+    plt.show()
     plt.savefig('/corral/utexas/hurricane/tgower/TempCorrelation/Corr_Coeffs_har_02/NC_Scatter')
     
     print('Plot Generated')
@@ -81,7 +80,7 @@ def main():
         time[i] = ctp[i]['Time(hrs)']
     
     #Generate Plot
-    create_plot(time, coeff)
+    create_plot(coeff, time)
 
 if __name__ == "__main__":
     """ This is executed when run from the command line """
